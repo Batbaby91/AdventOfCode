513d84efa158f7e868fa791e0fdc70b488c1e3b4
Batbaby91 <chipped_wood@hotmail.com>
2021-12-02 06:22:27 +0000

    2021 Day 2 complete

---
+++ AOC2021/Day2/Part1/Instruction.java
@@ -0,0 +1,15 @@
+package AOC2021.Day2.Part1;
+
+public class Instruction {
+  private String direction;
+  private int amount;
+
+  public Instruction (String instruction) {
+    String[] separated = instruction.split("\\s");
+    direction = separated[0];
+    amount = Integer.parseInt(separated[1]);
+  }
+
+  public String getDirection() { return direction; }  
+  public int getAmount() { return amount; }
+}
---
+++ AOC2021/Day2/Part2/Main.java
@@ -0,0 +1,58 @@
+package AOC2021.Day2.Part2;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+  public static ArrayList<Instruction> readInput(ArrayList<Instruction> instructions, String filepath) {
+    try {
+      File input = new File(filepath);    
+      Scanner reader = new Scanner(input);
+
+      while (reader.hasNextLine()) {
+        String line = reader.nextLine();
+        instructions.add(new Instruction(line));
+      }
+    }
+    catch (Exception e) { System.out.println(e.getMessage()); }
+
+    return instructions;
+  }
+
+  public static void main (String[] args) {
+    ArrayList<Instruction> instructions = new ArrayList<Instruction>();
+    String filepath = "AOC2021\\Day2\\Part2\\input.txt";
+    int horizontal = 0;
+    int depth = 0;
+    int aim = 0;
+    int result = 0;
+
+    instructions = readInput(instructions, filepath);
+
+    for(Instruction instruction : instructions) {
+      switch(instruction.getDirection()) {
+        case "forward": 
+          horizontal += instruction.getAmount();
+          depth += (aim * instruction.getAmount());
+          break;
+        case "down": 
+          aim += instruction.getAmount();
+          break;
+        case "up": 
+          aim -= instruction.getAmount();
+          break;
+        default: 
+          System.out.println(instruction.getDirection() + " " + instruction.getAmount()); 
+          break;
+      }
+    }
+
+    result = horizontal * depth;
+
+    System.out.println("horizontal: " + horizontal);
+    System.out.println("depth: " + depth);
+    System.out.println("result: " + result);
+  
+  }
+}
